37004e111eeb4e5761d47481ab39d7d241cbc0e8
Diego Yanez <diegoyanez@MacBook-Pro-9.fios-router.home>
2019-05-07 11:02:27 -0400

    add passport middleware & route

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
+
+const passport = require("passport")
+const users = require("./routes/api/users")
+
 require('dotenv').config();
 
 const app = express();
@@ -15,20 +19,31 @@ app.use(
     })
 );
 
+app.use(bodyParser.json());
+
 //DB Config
 
 mongoose.connect(process.env.DB, {useNewUrlParser: true})
     .then(() => console.log("MONGODB Connected"))
     .catch(err => console.log(err));
 
+// passport middleware
+app.use(passport.initialize());
+
+// passport config
+require("./config/passport")(passport)
+
+
+// Routes
+app.use("/api/users", users)
+
+
     // mongoose.Promise = global.Promise  not needed with mongoose 5.0 and up
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next()
-});
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next()
+// });
 
-app.use(bodyParser.json());
-
-app.listen( port, () => console.log(`listening on ${port}!`))
+app.listen( port, () => console.log(`You are on ${port}!`))
